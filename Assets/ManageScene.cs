ce8b5f16c8cf2eccab55b12b80ceaca2ec9e47ef
wonjaegang <52266110+wonjaegang@users.noreply.github.com>
2023-04-16 16:36:08 +0900

    Add Store Scene
    상점 씬 추가, 이전 씬 이동 함수 추가

--- Assets/ManageScene.cs
+++ Assets/ManageScene.cs
@@ -4,25 +4,41 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ManageScene : MonoBehaviour
-{   
+{
+    public static string LastScene = "HomeScene";
+
     public void LoadHomeScene()
     {
+        LastScene = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene("HomeScene");
     }
     public void LoadChapterScene()
     {
+        LastScene = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene("ChapterScene");
     }
     public void LoadLevelScene()
     {
+        LastScene = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene("LevelScene");
     }
     public void LoadInGameScene()
     {
+        LastScene = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene("InGameScene");
     }
     public void LoadOptionScene()
     {
+        LastScene = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene("OptionScene");
+    }
+    public void LoadStoreScene()
+    {
+        LastScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene("StoreScene");
+    }
+    public void LoadLastScene()
+    {
+        SceneManager.LoadScene(LastScene);
     }
 }
