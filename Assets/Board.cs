c015879a598b89a5e4949d5acd08b2141a635331
wonjaegang <52266110+wonjaegang@users.noreply.github.com>
2023-04-24 00:19:07 +0900

    Update DrawBoard(): Color
    DrawBoard() 메소드에 배경/수직선/마커 색 출력 기능 추가

--- Assets/Board.cs
+++ Assets/Board.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Board : MonoBehaviour
+/*
+    보드 입출력 관리 클래스
+ */
 {
     public List<List<string>> Data;
+    public string[] ColorCombination;
+
     public GameObject Marker;
     public GameObject VerticalLine;
 
@@ -23,31 +28,46 @@ public class Board : MonoBehaviour
         CameraWidth = CameraSize.x;
         CameraHeight = CameraSize.y;
     }
+    private Color GetColorFromHTML(string HTMLCode)
+    {
+        Color color;
+        ColorUtility.TryParseHtmlString(HTMLCode, out color);
+        return color;
+    }
 
     public void DrawBoard()
     {
         GetCameraSize();
+        Color BackGroundColor = GetColorFromHTML(ColorCombination[7]);
+        Camera.main.backgroundColor = BackGroundColor;
 
         int VerticalLineNum = Data[0].Count;
         for (int column = 0; column < VerticalLineNum; column++)
         {
-
-            Debug.Log(CameraWidth);
             float Pos_x = -CameraWidth + BoardSideMargin +
                           (CameraWidth - BoardSideMargin) * 2 / (VerticalLineNum - 1) * column;
 
             // VerticalLine 생성
             GameObject NewVerticalLine = Instantiate(VerticalLine, this.transform);
-            NewVerticalLine.transform.position = new Vector3(Pos_x,
-                                                             VerticalLineY,
-                                                             0);
-            // Marker 생성
-            float MarkerY = VerticalLineY + VerticalLineLength / 2 + MarkerOffset;
-            GameObject NewMarkerIn = Instantiate(Marker, this.transform);
-            NewMarkerIn.transform.position = new Vector3(Pos_x, MarkerY, 0);
+            NewVerticalLine.transform.position = new Vector3(Pos_x, VerticalLineY, 0);
+            Color VerticalLineColor = GetColorFromHTML(ColorCombination[6]);
+            NewVerticalLine.GetComponent<VerticalLineColor>().SetVerticalLineColor(VerticalLineColor);
 
-            GameObject NewMarkerOut = Instantiate(Marker, this.transform);
-            NewMarkerOut.transform.position = new Vector3(Pos_x, -MarkerY, 0);
+            // 입/출력 Marker 생성
+            float MarkerY = VerticalLineY + VerticalLineLength / 2 + MarkerOffset;
+            foreach (int gain in new int[2] {1, -1})
+            {
+                // 데이터에서 마커의 행 위치/값(색) 확인
+                int MarkerRow = (Data.Count - 1) * (1 - gain) / 2;
+                int DataValue = int.Parse(Data[MarkerRow][column]);
+                Color MarkerColor = GetColorFromHTML(ColorCombination[DataValue]);
+
+                // 마커 생성, 위치 및 색 부여
+                GameObject NewMarker = Instantiate(Marker, this.transform);
+                NewMarker.transform.position = new Vector3(Pos_x, gain * MarkerY, 0);
+                NewMarker.GetComponent<SpriteRenderer>().color = MarkerColor;
+            }            
+
 
             // 기타 Object 생성
             int HorizontalObjNum = Data.Count;
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,16 +7,32 @@ public class GameManager : MonoBehaviour
     인게임 내 전체 프로세스 관리 클래스
  */
 {
-    public List<List<string>> BoardData = new(){new List<string>() {  "1",   "2",   "3",   "4"},
-                                                new List<string>() {   "", "nr0", "nl0",    ""},
-                                                new List<string>() {  "3",   "2",   "1",   "4"}};
-
+    public List<List<string>> BoardData;
+    public string[] ColorCombination;
     public Board newBoard;
 
     // Start is called before the first frame update
     void Start()
     {
+        BoardData = new()
+        {
+            new List<string>() {  "1",   "2",   "3",   "4" },
+            new List<string>() {   "", "nr0", "nl0",    "" },
+            new List<string>() {  "3",   "2",   "1",   "4" }
+        };
+
+        ColorCombination = new string[8]{ "#197278",  // 마커 1
+                                          "#83a8a6",  // 마커 2
+                                          "#ae9d96",  // 마커 3
+                                          "#d99185",  // 마커 4
+                                          "#c44536",  // 마커 5
+                                          "#772e25",  // 마커 6
+                                          "#283d3b",  // 수직선
+                                          "#edddd4"}; // 배경
+
         newBoard.Data = BoardData;
+        newBoard.ColorCombination = ColorCombination;
+
         newBoard.DrawBoard();
     }
 
